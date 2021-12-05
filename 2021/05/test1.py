24d7188448aa4388f92bc9bb073deca838e6cf36
Matthieu Serrepuy <matthieu@serrepuy.fr>
2021-12-05 09:34:02 +0100

    2021-05: Part 1

---
+++ 2021/05/test1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+from collections import Counter
+
+def load_data_from_file(filename):
+    """ Load the input from a file and return the transformed version """
+    with open(filename, 'r') as f:
+        data = list(map(prepare_data, f.readlines()))
+    return data
+
+
+def prepare_data(x):
+    """ transform the data the way we want it for the puzzle """
+    p1,p2 = x.strip().split(' -> ')
+    return (
+            tuple(map(int,p1.split(','))), 
+            tuple(map(int,p2.split(',')))
+            )
+
+
+class Vent:
+    """ Define a vent line """
+    def __init__(self, coords):
+        p1, p2 = coords
+        self.p1x = p1[0]
+        self.p1y = p1[1]
+        self.p2x = p2[0]
+        self.p2y = p2[1]
+
+    def __str__(self):
+        return f"({self.p1x}, {self.p1y}) -> ({self.p2x},{self.p2y})"
+
+    @property
+    def is_horizontal(self):
+        return self.p1y == self.p2y
+
+    @property
+    def is_vertical(self):
+        return self.p1x == self.p2x
+
+    @property
+    def is_diagonal(self):
+        return not self.is_horizontal and not self.is_vertical
+
+    def dangerous_points(self):
+        print(self)
+        points = []
+        for x in range(min(self.p1x,self.p2x), max(self.p1x,self.p2x)+1):
+            for y in range(min(self.p1y,self.p2y), max(self.p1y,self.p2y)+1):
+                points.append((x,y))
+        return points
+
+def solve(data):
+    """ Solve the puzzle and return the solution """
+    dangerous_points = []
+    for vent_coord in data:
+        v = Vent(vent_coord)
+        if v.is_diagonal:
+            print(f"{vent_coord} is a diagonal. Ignoring it")
+            continue
+        dangerous_points.extend(v.dangerous_points())
+    return len(list(filter(lambda x: x > 1, Counter(dangerous_points).values())))
+
+
+print("--> test data <--")
+test_input = load_data_from_file('test_input')
+assert solve(test_input) == 5
+
+print()
+print("--> real data <--")
+my_input = load_data_from_file('input')
+print(f"\nanswer: {solve(my_input)}")
