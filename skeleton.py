f33cedc7adc22d153f7252bab0dd7dec03ca2051
Matthieu Serrepuy <matthieu@serrepuy.fr>
2023-12-02 09:18:32 +0100

    Update skelelton script to strip data

--- skeleton.py
+++ skeleton.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 def load_data_from_file(filename):
-    """ Load the input from a file and return the transformed version """
-    with open(filename, 'r') as f:
-        data = list(map(prepare_data, f.readlines()))
-    return data
+  """Load the input from a file and return the transformed version"""
+  with open(filename, "r") as f:
+    data = list(map(prepare_data, f.readlines()))
+  return data
 
 
 def prepare_data(x):
-    """ transform the data the way we want it for the puzzle """
-    return x
+  """transform the data the way we want it for the puzzle"""
+  return x.strip()
 
 
 def solve(data):
-    """ Solve the puzzle and return the solution """
-    return False
+  """Solve the puzzle and return the solution"""
+  return False
 
 
 print("--> test data <--")
-test_input = load_data_from_file('test_input')
+test_input = load_data_from_file("test_input")
 assert solve(test_input) == 273
 
 print()
 print("--> real data <--")
-my_input = load_data_from_file('input')
+my_input = load_data_from_file("input")
 print(f"\nanswer: {solve(my_input)}")
