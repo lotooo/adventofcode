a50e1ab25161dc7d1a9992b0ecb193a8b44c65ac
Matthieu Serrepuy <matthieu@serrepuy.fr>
2024-12-06 23:51:15 +0100

    fixup! 2024-06: broken part2

--- 2024/06/test2.py
+++ 2024/06/test2.py
@@ -18,7 +18,7 @@ def prepare_data(x):
     return x.strip()
 
 
-def test_path(data, x, y):
+def test_path(data, x, y, debug=False):
     m = Grid2D(data)
     my_way = [(x, y)]
     orders = "URDL"
@@ -33,9 +33,12 @@ def test_path(data, x, y):
             )
         )
         my_way.extend(nexts)
-        if nexts == m.get_nexts(x, y, orders[i % 4]):
+        if len(nexts) == len(m.get_nexts(x, y, orders[i % 4])):
             return True
-        if (nexts, orders[i % 4]) in walked:
+        if (nexts, orders[i % 4]) in walked and len(nexts) > 0:
+            if debug:
+                print((nexts, orders[i % 4]))
+                m.print(my_way)
             return False
         walked.append((nexts, orders[i % 4]))
 
@@ -62,7 +65,7 @@ def solve(data):
     initial_path = set(my_way)
     for new_obstruction in [(x, y) for x, y in initial_path if (x, y) != (sx, sy)]:
         new_test = data.copy()
-        tmp = list(data[new_obstruction[1]])
+        tmp = list(new_test[new_obstruction[1]])
         tmp[new_obstruction[0]] = "#"
         new_test[new_obstruction[1]] = "".join(tmp)
         if not test_path(new_test, sx, sy):
